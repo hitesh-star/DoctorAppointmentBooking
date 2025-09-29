8c49a3d069dc93b7aaac940e249181a452c88381
Hitesh <hitesh.parashar2016@gmail.com>
2025-09-29 17:14:16 +0530

    Navbar component

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        {/*----Left Side -------- */}
+        <div>
+          <p>
+            Book Appointment <br/>
+          </p>
+        </div>
+         {/*----Right Side -------- */}
+         <div>
+
+         </div>
+    </div>
+  )
+}
+
+export default Header
