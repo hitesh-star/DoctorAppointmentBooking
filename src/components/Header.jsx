54b6d4e8f2906331a4fa9c95ea6d0d4ecce0aded
Hitesh <hitesh.parashar2016@gmail.com>
2025-09-29 17:49:40 +0530

    Header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div>
-        {/*----Left Side -------- */}
-        <div>
-          <p>
-            Book Appointment <br/>
-          </p>
+    <div className='flex flex-col md:flex-row flex-wrap bg-[#5f6FFF] rounded-lg px-6 md:px-10 lg:px-20'>
+      {/*----Left Side -------- */}
+      <div className='md:W-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
+        <p className='text-3xl md:text-4xl mg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
+          Book Appointment <br />With Trusted Doctors
+        </p>
+        <div className='flex flex-col flex-row items-center gap-3 text-white text-sm font-light'>
+          <img className='w-28' src={assets.group_profiles} alt='' />
+          <p>Simply Browse through our extensive list of trusted doctors,<br className='hidden sm:block' />schedule your appointment hassle-free.</p>
         </div>
-         {/*----Right Side -------- */}
-         <div>
+        <a href='#speciality' className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>
+          Book appointment <img className='w-3' src={assets.arrow_icon} alt='' />
+        </a>
+      </div>
 
-         </div>
+      {/*----Right Side -------- */}
+      <div className='md:w-1/2 relative '>
+        <img src={assets.header_img} alt='' className='w-full md:Absolute bottom-0 h-auto rounded-lg'/>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,19 +13,19 @@ const Navbar = () => {
             <ul className='hidden md:flex items-start gap-5 font-medium'>
                 <NavLink to='/'>
                     <li className='py-1'>HOME</li>
-                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+                    <hr className='border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden' />
                 </NavLink>
                 <NavLink to='/doctors'>
                     <li className='py-1'>ALL DOCTORS</li>
-                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+                    <hr className='border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden' />
                 </NavLink>
                 <NavLink to='/about'>
                     <li className='py-1'>ABOUT</li>
-                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+                    <hr className='border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden' />
                 </NavLink>
                 <NavLink to='/contact'>
                     <li className='py-1'>CONTACT</li>
-                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+                    <hr className='border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden' />
                 </NavLink>
             </ul>
             <div className='flex items-center gap-4'>
